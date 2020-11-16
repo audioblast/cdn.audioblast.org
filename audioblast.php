ac881f3ee71c283a2d96c5ac0fb959162b4aa469
Ed Baker <edwbaker@gmail.com>
2020-11-16 14:49:27 +0000

    Create audioblast.php

---
+++ audioblast.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<title>audioBLAST! Content delivery network</title>
+</head>
+
+<body>
+
+  <p align="center"<img src="audioblast_logo.png" /></p>
+  <h1 align="center">audioBLAST! Content delivery network</h1>
+
+  <h2>audioBLAST! Resources</h2>
+  <ul>
+    <li><a href="audioblast_logo.png">audioBLAST! logo</a></li>
+  </ul>
+
+</body>
+
+</html>
