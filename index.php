9fe71b4e9576bf5bef355a3b770650f90300f8c9
Ed Baker <edwbaker@gmail.com>
2020-11-16 14:48:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,21 @@
 </head>
 
 <body>
-<h2>audioBLAST! Content delivery network</h2>
 
-<p>This repository contains the resources hosted on cdn.audioblast.org. These resources may be specific to the audioBLAST! project, or be of general acoustic or bioacoustic utility.</p>
+  <p align="center"<img src="audioblast_logo.png" /></p>
+  <h1 align="center">audioBLAST! Content delivery network</h1>
 
-<p>Third parties are generally welcome to make use of this CDN, although if large loads are expected please contact us first.</p>
+  <p>This repository contains the resources hosted on cdn.audioblast.org. These resources may be specific to the audioBLAST! project, or be of general acoustic or bioacoustic utility.</p>
+
+  <p>Third parties are generally welcome to make use of this CDN, although if large loads are expected please contact us first.</p>
+  
+  <h2>Resources</h2>
+  <ul>
+    <li><a href="audioblast.php">audioBLAST! resources</a></li>
+    <li><a href="jPlayer">jPlayer</a></li>
+    <li><a href="wavesurfer">wavesurfer</a></li>
+    <li><a href="zcjs">zcjs</a><li>
+  </ul>
 
 </body>
 
