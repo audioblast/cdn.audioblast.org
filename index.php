652e238f8112e256ad3aca48e73829e5fc3f428b
Ed Baker <edwbaker@gmail.com>
2020-11-16 14:36:09 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+<title>audioBLAST! Content delivery network</title>
+</head>
+
+<body>
+<h2>audioBLAST! Content delivery network</h2>
+
+<p>This repository contains the resources hosted on cdn.audioblast.org. These resources may be specific to the audioBLAST! project, or be of general acoustic or bioacoustic utility.</p>
+
+<p>Third parties are generally welcome to make use of this CDN, although if large loads are expected please contact us first.</p>
+
+</body>
+
+</html>
