3e66078562e5e828664e15ada82e15d033629c6f
Ed Baker <edwbaker@gmail.com>
2020-11-23 13:08:32 +0000

    Add tabulator to index page.

--- index.php
+++ index.php
@@ -12,15 +12,16 @@
   <p>This repository contains the resources hosted on cdn.audioblast.org. These resources may be specific to the audioBLAST! project, or be of general acoustic or bioacoustic utility.</p>
 
   <p>Third parties are generally welcome to make use of this CDN, although if large loads are expected please contact us first.</p>
-  
+
   <h2>Resources</h2>
   <ul>
     <li><a href="audioblast.php">audioBLAST! resources</a></li>
     <li><a href="jPlayer">jPlayer</a></li>
+    <li><a href="tabulator">Tabulator</a></li>
     <li><a href="wavesurfer">wavesurfer</a></li>
     <li><a href="zcjs">zcjs</a></li>
   </ul>
-  
+
   <p>Requests and enquiries should be made via a <a href="https://github.com/audioblast/cdn.audioblast.org">GitHub issue</a>.</p>
 
 </body>
